993fb026dbb425698e2dfb4126c45db6ac4c9fcc
Sebastian Villada Jaramillo <sebastian.villada.jaramillo@gmail.com.co>
2024-04-06 17:46:39 -0500

    first commit

---
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.agenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.agenda.db.DbHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnCrear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnCrear.findViewById(R.id.btnCrear);
+
+        btnCrear=findViewById(R.id.btnCrear);
+        btnCrear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DbHelper dbHelper = new DbHelper(MainActivity.this);
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                if(db!=null){
+                    Toast.makeText(MainActivity.this,"BASE DE DATOS CREADA", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this,"ERROR AL CREAR BASE DE DATOS", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
